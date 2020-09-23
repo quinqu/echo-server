ad831ba1d9e8537a7e371921dc4d25782323b543
Quin Quintero <quinq@gravitational.com>
2020-09-23 15:12:44 -0700

    testing

--- server/echoserver_test.go
+++ server/echoserver_test.go
@@ -16,9 +16,12 @@ func TestEcho(t *testing.T) {
 		t.Fatal(err)
 	}
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(echo)
-	handler.ServeHTTP(rr, req)
+	
+	echo(rr, req)
 	bodyBytes, err := ioutil.ReadAll(rr.Body)
+	if err != nil {
+		t.Errorf("error reading data: %v", err)
+	}
 	response := string(bodyBytes)
 	if response != message {
 		t.Errorf("response does not match request: got %v, want: %v", response, message)
