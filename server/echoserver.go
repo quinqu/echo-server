cac6b21fa0f7ad28e1375805822ffa1a4e6904df
Quin Quintero <quinq@gravitational.com>
2020-09-23 17:12:34 -0700

    set up tls

--- server/echoserver.go
+++ server/echoserver.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net"
 	"net/http"
 	"os"
 
@@ -16,13 +15,17 @@ import (
 var authToken string
 
 func echo(w http.ResponseWriter, r *http.Request) {
+
 	var response string
+	sem <- struct{}{}
+	defer func() { <-sem }()
 	out, err := ioutil.ReadAll(r.Body)
 	response = string(out)
 	if err != nil {
 		response = fmt.Sprintf("there was error reading request: " + err.Error())
 	}
 	fmt.Fprintf(w, response)
+
 }
 
 func isAuthorized(endpoint func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
@@ -51,9 +54,12 @@ func generateToken(n int) (string, error) {
 var (
 	app  = kingpin.New("echoserver", "echo server will echo a clients request")
 	port = app.Flag("port", "port to bind to").Required().String()
+	sem = make(chan struct{}, 20)
 )
 
+
 func main() {
+	
 	token, err := generateToken(20)
 	if err != nil {
 		log.Fatal("unable to generate token", err)
@@ -62,14 +68,17 @@ func main() {
 	fmt.Println(authToken)
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 	serverPort := *port
-	listener, err := net.Listen("tcp", ":"+serverPort)
-	if err != nil {
-		log.Fatal(err)
-	}
-	http.HandleFunc("/echo", isAuthorized(echo))
-	log.Println("Listening on localhost:" + serverPort)
-	err = http.Serve(listener, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+
+	http.HandleFunc("/echo", echo)//isAuthorized(echo))
+
+	// ListenAndServe invokes Serve, which calls Accept in a loop. 
+	// Accept is similar to the socket accept syscall, creating a new 
+	// connection whenever a new client is accepted; the connection is then 
+	// used for interacting with the client. This connection is type conn in the 
+	// server.go file, a private type with server state for each client connection. 
+	// Its serve method actually serves a connection, pretty much as you would expect; 
+	// it reads some data from the client and invokes the user-suppied handler depending on the path.
+
+	log.Fatal(http.ListenAndServeTLS(":"+serverPort, "localhost.crt", "localhost.key", nil))
+
 }
