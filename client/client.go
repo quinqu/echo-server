fc58701e9c5446e9b1f380bb32324dc7b4e27586
Quin Quintero <quinq@gravitational.com>
2020-09-25 08:20:18 -0700

    request in method

--- client/client.go
+++ client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -21,47 +22,49 @@ var (
 
 func main() {
 	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
-
 	case send.FullCommand():
-		requestMessage := strings.Join(*message, " ")
-		reader := strings.NewReader(requestMessage)
-		endpoint := *host + "/echo"
-		req, err := http.NewRequest("POST", endpoint, reader)
+		err := request()
 		if err != nil {
-			fmt.Println("could not resolve host: ", err)
 			os.Exit(1)
-			return
 		}
-		req.Header.Add("Token", *token)
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Println("request failed: ", err)
-			os.Exit(1)
-			return
-		}
-
-		defer resp.Body.Close()
-		if resp.StatusCode != http.StatusOK {
-			fmt.Println("Not OK HTTP status:", resp.StatusCode)
-			os.Exit(1)
-			return
-		}
-
-		bodyBytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println("could not read body: ", err)
-			os.Exit(1)
-			return
-		}
-
-		serverOutput := string(bodyBytes)
-		if serverOutput != requestMessage {
-			fmt.Println(*host + " did not echo your request, oops! got:")
-		}
-		fmt.Println(serverOutput)
 	default:
-
 		fmt.Println("command not given")
 	}
+}
+
+func request() error {
+	requestMessage := strings.Join(*message, " ")
+	reader := strings.NewReader(requestMessage)
+	endpoint := *host + "/echo"
+	req, err := http.NewRequest("POST", endpoint, reader)
+	if err != nil {
+		fmt.Println("could not resolve host: ", err)
+		return err
+	}
+	req.Header.Add("Token", *token)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("request failed: ", err)
+		return err
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Not OK HTTP status:", resp.StatusCode)
+		return errors.New("Not OK HTTP Status")
+	}
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("could not read body: ", err)
+		return err
+	}
+
+	serverOutput := string(bodyBytes)
+	if serverOutput != requestMessage {
+		fmt.Println(*host + " did not echo your request, oops! got:")
+	}
+	fmt.Println(serverOutput)
+	return nil
 }
